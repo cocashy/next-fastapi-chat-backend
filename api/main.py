c8c659caf4e243849ffc68158393648ee61fd4db
cocashy <tomo170119@gmail.com>
2024-10-17 23:53:49 +0900

    Update CORS origins to allow localhost and deployed frontend

--- api/main.py
+++ api/main.py
@@ -17,7 +17,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://next-fastapi-chat-frontend.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
